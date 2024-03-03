f1340df2438ae8e09156245172a76903a0294101
4tnajtowcy <161967347+4tnajtowcy@users.noreply.github.com>
2024-03-03 14:16:43 +0100

    GOTOWY APP

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,28 @@
-import start from "../../config/start/start.js"
+const log = console.log
 
-start.init()
+import configFile from "../../config/config-var";
+const { token, prefix, clientid, guildid } = configFile;
+
+import { Client, GatewayIntentBits, Events } from "discord.js";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on(Events.ClientReady, (c) => {
+  log(`Zalogowano jako ${c.user.tag}`);
+});
+
+client.on(Events.MessageCreate, (msg) => {
+  if (msg.author.bot) return;
+
+  if (msg.content === `${prefix}ping`) {
+    msg.reply(`Pong!\n\nClient ping: ${client.ws.ping}`);
+  };
+});
+
+client.login(token)
