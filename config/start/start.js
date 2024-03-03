cea5a1d45c72875c79c5a77295ee4c8cd3b3a054
4tnajtowcy <161967347+4tnajtowcy@users.noreply.github.com>
2024-03-03 13:46:56 +0100

    Update start.js

--- config/start/start.js
+++ config/start/start.js
@@ -1,5 +1,3 @@
-const chalk = require("chalk");
-
-console.log(chalk.green("Reading the script..."));
-console.log(chalk.blue("Searching the token..."));
-console.log(chalk.yellow("\nCREATED BY: 4tnajtowcy\n"));
+export default {
+  
+}
