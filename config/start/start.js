f54f61c36df61bb8204e52807cfedba6dd635569
ojando14 <fortnajtowcy54@gmail.com>
2024-03-03 13:48:35 +0100

    DELETED PACK/START

--- config/start/start.js
+++
@@ -1,5 +0,0 @@
-const chalk = require("chalk");
-
-console.log(chalk.green("Reading the script..."));
-console.log(chalk.blue("Searching the token..."));
-console.log(chalk.yellow("\nCREATED BY: 4tnajtowcy\n"));
