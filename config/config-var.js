ae9860e62bbd21d32fe8d871167af45438d0c5db
4tnajtowcy <161967347+4tnajtowcy@users.noreply.github.com>
2024-03-03 14:17:09 +0100

    Update config-var.js

--- config/config-var.js
+++ config/config-var.js
@@ -1,7 +1,6 @@
 module.exports =  {
+  prefix: "",
   token: "",
   clientid: "",
   guildid: "",
 }
-
-// Jeżeli w package.json jest napisałeś "type": "module" to zmień 1 linijke (całą) na export default {
